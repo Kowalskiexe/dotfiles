22fb4e0f6f9a1200cbcbc1bb9a4c651570c74423
Maciej Kowalski <kowalski.exe@gmail.com>
2025-03-16 19:18:59 +0100

    add yazi

---
+++ .config/yazi/init.lua
@@ -0,0 +1,17 @@
+require("bookmarks"):setup({
+	last_directory = { enable = true, persist = true },
+	persist = "all",
+	desc_format = "full",
+	file_pick_mode = "hover",
+	notify = {
+		enable = true,
+		timeout = 1,
+		message = {
+			new = "New bookmark '<key>' -> '<folder>'",
+			delete = "Deleted bookmark in '<key>'",
+			delete_all = "Deleted all bookmarks",
+		},
+	},
+})
+
+require("relative-motions"):setup({ show_numbers="relative", show_motion = true })
