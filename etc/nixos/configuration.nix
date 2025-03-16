41b71a60210270ec40315e49665c3a85e7f22ac7
Maciej Kowalski <kowalski.exe@gmail.com>
2025-03-16 19:16:48 +0100

    dump configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -3,7 +3,6 @@
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
 { config, lib, pkgs, ... }:
-
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -15,19 +14,37 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  systemd.services.dlm.wantedBy = [ "multi-user.target" ];
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+
+  i18n.supportedLocales = [
+      "C.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "pl_PL.UTF-8/UTF-8"
+    ];
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    evdi
+  ];
 
-  networking.hostName = "t470"; # Define your hostname.
+  networking.hostName = "x1"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
   services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn;
+
+  services.fwupd.enable = true;
+
+  services.hardware.bolt.enable = true;
 
   # services.pgadmin = {
   #   enable = true;
@@ -39,12 +56,16 @@
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
+  documentation.dev.enable = true; # install man pages
+
   hardware = {
+    spacenavd.enable = true;
+    i2c.enable = true;
     graphics = {
       enable = true;
       extraPackages = with pkgs; [
         intel-compute-runtime
-	intel-media-driver
+        intel-media-driver
       ];
     };
     bluetooth = {
@@ -75,13 +96,13 @@
   # };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.videoDrivers = [ "modesetting" ];
+  services.xserver.enable = false;
+  services.xserver.videoDrivers = [ "displaylink" ];
   # services.xserver.displayManager.gdm = {
   #   enable = true;
   #   wayland = true;
   # };
-  services.displayManager.enable = true;
+  # services.displayManager.enable = false;
   # services.displayManager.ly.enable = true;
   # services.displayManager.sddm = {
   #   enable = true;
@@ -95,6 +116,8 @@
   services.tailscale.enable = true;
 
   services.onedrive.enable = true;
+
+  services.syncthing.enable = true;
 
   services.kanata = {
     enable = true;
@@ -128,26 +151,40 @@
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
+  security.rtkit.enable = true;
   # Enable sound.
   # hardware.pulseaudio.enable = true;
   # OR
   services.pipewire = {
     enable = true;
-    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    # pulse.enable = true;
+    jack.enable = true;
   };
 
 
+  programs.virt-manager.enable = true;
+
   # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
   virtualisation = {
-    podman = {
+    containers.enable = true;
+    libvirtd = {
       enable = true;
+    };
+    spiceUSBRedirection.enable = true;
+    podman = {
+      enable = false;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
+    };
+    docker = {
+      enable = true;
+      storageDriver = "btrfs";
     };
   };
 
@@ -157,9 +194,10 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.inter = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "networkmanager" "uinput" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      flatpak
+    extraGroups = [ "wheel" "networkmanager" "uinput" "docker" "libvirtd"]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keys = [
+      # yeah, that's my public key
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILc/IQW+eGrh1MWxi86PxrIVcKjNj/udTuMWhq/lCuy/ kowalski.exe@gmail.com"
     ];
   };
 
@@ -174,25 +212,22 @@
     mako
     libnotify
     eww
-    sddm
     wofi
-    home-manager
     kitty
     git
     zip
     unzip
     fish
     nushell
-    # libgccjit
     libgcc
     gcc
     zlib
-    linuxKernel.packages.linux_xanmod.tp_smapi
+    linuxKernel.packages.linux_xanmod.tp_smapi  # ThinkPad only
     libsForQt5.qt5.qtquickcontrols2
     libsForQt5.qt5.qtgraphicaleffects
-    python312Packages.numpy
-    python312Packages.setuptools
-    python3
+    (python3.withPackages(ps: with ps; [ numpy setuptools ]))
+    kotlin
+    gradle
     psmisc
     arp-scan
     usbutils
@@ -207,18 +242,23 @@
     xwayland
     networkmanagerapplet
     clang
-    rocmPackages.llvm.clang-tools-extra # TODO: change
     bottles
     atuin
     zoxide
+    gnupg
+    pinentry-all
+    virtualglLib
+    rpi-imager
+    hyprpolkitagent
   ];
   environment = {
     sessionVariables = {
       NIXOS_OZONE_WL = "1";
-      # LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
     };
   };
-
+  environment.variables = {    
+    WLR_EVDI_RENDER_DEVICE = "/dev/dri/card1";                                                                                                   
+  };
 
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
@@ -256,12 +296,14 @@
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
 
+  programs.java.enable = true;
+
   security.pam.services.swaylock = {};
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
